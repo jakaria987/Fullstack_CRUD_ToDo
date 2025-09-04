88d5a2226c4b13b6a0bdc41a934d29ed80451d36
jakaria987 <jakhossain999@gmail.com>
2025-09-04 12:29:07 +0600

    integrate mongodb and set up post and get routing

--- server/index.js
+++ server/index.js
@@ -1,6 +1,25 @@
 const express = require("express");
+const { default: mongoose } = require("mongoose");
 const app = express();
 require("dotenv").config();
+
+// Database connection
+mongoose
+  .connect(process.env.DB_URL)
+  .then(() => {
+    console.log("Database connected");
+  })
+  .catch((err) => {
+    console.log(err.message || err);
+  });
+
+// route define
+app.post("/addTodo", (req, res) => {
+  res.status(201).json({ success: true, message: "Todo created successfully" });
+});
+app.get("/getTodo", (req, res) => {
+  res.status(200).json({ success: true, message: "Todo fetch successfully" });
+});
 
 app.listen(process.env.PORT, () => {
   console.log(`Server is running on port ${process.env.PORT}`);
